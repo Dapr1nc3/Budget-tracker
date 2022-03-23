a38703331f29b98c4939662d4a595e5462cb895e
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-22 22:22:21 -0400

    adjusting the manifest json to fix deployment

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI =
-  (process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget";
 
 const app = express();
 
